1affd4781b9144a80240fb705585dee0992e3cd8
Caden Swanson <ctswanny24@gmail.com>
2025-09-05 21:47:19 -0500

    Current Finished Project 0 All checks done

--- Game1.cs
+++ Game1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System;
 
 // Game Project 0 Requirements:
 // At least 4 images rendered with spriteBatch
@@ -13,22 +14,24 @@ namespace GameProject_0
 {
     public class Game1 : Game
     {
-        private string _sillyMessage = "WOW, look at that timer go!!!";
+        private string gameTitle = "Night Drive";
 
-        private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
+        private GraphicsDeviceManager graphics;
+        private SpriteBatch spriteBatch;
+
         private Texture2D atlas;
+        private Texture2D moonSprite;
+        private Texture2D streetLampSprite;
 
-        private Texture2D _movingSpriteTexture;
-        private Vector2 _movingSpritePosition;
-        private Vector2 _movingSpriteVelocity;
+        private Vector2 moonPosition;
+        private Vector2 moonVelocity;
 
-        private SpriteFont _arial;
-        VehicleSprite[] vehicles;
+        private SpriteFont arial;
+        private VehicleSprite vehicle;
 
         public Game1()
         {
-            _graphics = new GraphicsDeviceManager(this);
+            graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
@@ -36,26 +39,26 @@ namespace GameProject_0
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-            _movingSpritePosition = new Vector2((_graphics.GraphicsDevice.Viewport.Width - 16) / 2, (_graphics.GraphicsDevice.Viewport.Height - 16) / 2);
-
-            vehicles = new VehicleSprite[]
-            {
-                new VehicleSprite(){ Position = new Vector2(50, 100), Direction = Direction.Right },
-                new VehicleSprite(){ Position = new Vector2(100, 150), Direction = Direction.Down },
-                new VehicleSprite(){ Position = new Vector2(75, 300), Direction = Direction.Up },
-            };
+            moonPosition = new Vector2(0, 30);
+            moonVelocity = new Vector2(.01f, 0);
+            moonVelocity.Normalize();
+            moonVelocity *= 100;
+            vehicle = new VehicleSprite() {Position =  new Vector2(0, graphics.GraphicsDevice.Viewport.Height - 92), Direction = Direction.Right, restartLocation = new Vector2(-90, graphics.GraphicsDevice.Viewport.Height - 92), ScreenWidth =  graphics.GraphicsDevice.Viewport.Width };
 
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
-            _spriteBatch = new SpriteBatch(GraphicsDevice);
+            spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
-            foreach (var vehicle in vehicles) vehicle.LoadContent(Content);
+            vehicle.LoadContent(Content);
             atlas = Content.Load<Texture2D>("colored_packed");
-            _arial = Content.Load<SpriteFont>("arial");
+            arial = Content.Load<SpriteFont>("arial");
+            moonSprite = Content.Load<Texture2D>("Moon");
+            streetLampSprite = Content.Load<Texture2D>("Streetlamp");
+
         }
 
         protected override void Update(GameTime gameTime)
@@ -64,34 +67,47 @@ namespace GameProject_0
                 Exit();
 
             // TODO: Add your update logic here
-            foreach (var vehicle in vehicles) vehicle.Update(gameTime);
+
+            moonPosition += moonVelocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            if(moonPosition.X > graphics.GraphicsDevice.Viewport.Width)
+            {
+                moonPosition = new Vector2(-32, 30);
+            }
+
+            vehicle.Update(gameTime);
 
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.PaleVioletRed);
+            GraphicsDevice.Clear(Color.Black);
 
             // TODO: Add your drawing code here
-            _spriteBatch.Begin();
-            _spriteBatch.DrawString(_arial, $"You have been playing my game for {gameTime.TotalGameTime.TotalSeconds:f} seconds", new Vector2(2, 2), Color.Beige);
-            if(gameTime.TotalGameTime.TotalSeconds > 10.0)
+            spriteBatch.Begin();
+
+            spriteBatch.DrawString(arial, $"{gameTitle}", new Vector2((graphics.GraphicsDevice.Viewport.Width - arial.MeasureString(gameTitle).X) / 2, 100), Color.Yellow);
+            spriteBatch.DrawString(arial, $"Press 'Esc' key to exit the game", new Vector2((graphics.GraphicsDevice.Viewport.Width - arial.MeasureString("Press 'Esc' key to exit the game").X / 2) / 2,
+                graphics.GraphicsDevice.Viewport.Height - 50), Color.PaleGoldenrod, 0.0f, Vector2.Zero, 0.5f, SpriteEffects.None, 0.0f);
+
+            spriteBatch.Draw(moonSprite, moonPosition, Color.White);
+
+            for(int x = 0; x < graphics.GraphicsDevice.Viewport.Width; x += 100)
             {
-                _spriteBatch.DrawString(_arial, $"{_sillyMessage}",
-                    new Vector2((_graphics.GraphicsDevice.Viewport.Width - _arial.MeasureString(_sillyMessage).X) / 2,
-                        (_graphics.GraphicsDevice.Viewport.Height - _arial.MeasureString(_sillyMessage).Y) / 2),
-                    Color.Black);
+                spriteBatch.Draw(streetLampSprite, new Vector2(x, graphics.GraphicsDevice.Viewport.Height - 150), null, Color.White, 0.0f, Vector2.Zero, 2.0f, SpriteEffects.None, 0.0f);
             }
-            _spriteBatch.Draw(atlas, new Vector2(2, (_graphics.GraphicsDevice.Viewport.Height - 16) / 2), new Rectangle(192, 320, 32, 16), Color.Purple, 
-                0, Vector2.Zero, new Vector2(2.0f, 2.0f), SpriteEffects.None, 0.0f);
-            
-            _spriteBatch.Draw(atlas, _movingSpritePosition, new Rectangle(400, 112, 16, 16), Color.Purple,
-                0, Vector2.Zero, new Vector2(2.0f, 2.0f), SpriteEffects.None, 0.0f);
 
-            foreach (var vehicle in vehicles) vehicle.Draw(gameTime, _spriteBatch);
+            spriteBatch.Draw(atlas, new Vector2(50, graphics.GraphicsDevice.Viewport.Height - 86), new Rectangle(35 * 16, 21 * 16, 16, 16), Color.White);
+            spriteBatch.Draw(atlas, new Vector2(600, graphics.GraphicsDevice.Viewport.Height - 86), new Rectangle(36 * 16, 21 * 16, 16, 16), Color.White);
+            spriteBatch.Draw(atlas, new Vector2(200, graphics.GraphicsDevice.Viewport.Height - 86), new Rectangle(37 * 16, 21 * 16, 16, 16), Color.White);
 
-            _spriteBatch.End();
+
+
+            vehicle.Draw(gameTime, spriteBatch);
+
+
+            spriteBatch.End();
 
             base.Draw(gameTime);
         }
--- VehicleSprite.cs
+++ VehicleSprite.cs
@@ -19,7 +19,6 @@ namespace GameProject_0
 
     public class VehicleSprite
     {
-        private Random random;
 
         private Texture2D texture;
 
@@ -27,12 +26,11 @@ namespace GameProject_0
 
         public Direction Direction;
 
-        public Vector2 Position;
+        public Vector2 restartLocation;
 
-        public VehicleSprite()
-        {
-            random = new System.Random();
-        }
+        public Vector2 Position;
+        
+        public float ScreenWidth;
 
         /// <summary>
         /// Loads the bat sprite texture
@@ -40,7 +38,7 @@ namespace GameProject_0
         /// <param name="content">The content manager to load</param>
         public void LoadContent(ContentManager content)
         {
-            texture = content.Load<Texture2D>("colored_packed");
+            texture = content.Load<Texture2D>("Car");
         }
 
         /// <summary>
@@ -52,39 +50,24 @@ namespace GameProject_0
             //Update the direction timer
             directionTimer += gameTime.ElapsedGameTime.TotalSeconds;
 
-            if(directionTimer > 3.5)
+            if(Position.X > ScreenWidth)
             {
-                switch (Direction)
-                {
-                    case Direction.Up:
-                        Direction = Direction.Right;
-                        break;
-                    case Direction.Right:
-                        Direction = Direction.Down;
-                        break;
-                    case Direction.Down:
-                        Direction = Direction.Left;
-                        break;
-                    case Direction.Left:
-                        Direction = Direction.Up;
-                        break;
-                }
-                directionTimer -= 3.5;
+                Position = restartLocation;
             }
 
             switch (Direction)
             {
                 case Direction.Up:
-                    Position += new Vector2(0, -1) * random.NextInt64(100, 600) * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                    Position += new Vector2(0, -1) * 200 * (float)gameTime.ElapsedGameTime.TotalSeconds;
                     break;
                 case Direction.Down:
-                    Position += new Vector2(0, 1) * random.NextInt64(100, 600) * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                    Position += new Vector2(0, 1) * 200 * (float)gameTime.ElapsedGameTime.TotalSeconds;
                     break;
                 case Direction.Left:
-                    Position += new Vector2(-1, 0) * random.NextInt64(100, 600) * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                    Position += new Vector2(-1, 0) * 200 * (float)gameTime.ElapsedGameTime.TotalSeconds;
                     break;
                 case Direction.Right:
-                    Position += new Vector2(1, 0) * random.NextInt64(100, 600) * (float)gameTime.ElapsedGameTime.TotalSeconds;
+                    Position += new Vector2(1, 0) * 200 * (float)gameTime.ElapsedGameTime.TotalSeconds;
                     break;
             }
         }
@@ -96,15 +79,8 @@ namespace GameProject_0
         /// <param name="spriteBatch"> The SpriteBatch to draw with </param>
         public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
         {
-            //Using a truck sprite if the random number is odd
-            if(random.NextInt64() % 2 == 1)
-            {
-                spriteBatch.Draw(texture, Position, new Rectangle(192, 320, 32, 16), Color.Purple);
-            }
-            else
-            {
-                spriteBatch.Draw(texture, Position, new Rectangle(224, 336, 16, 16), Color.Crimson);
-            }
+            //Using a car sprite if the random number is odd
+                spriteBatch.Draw(texture, Position, Color.Purple);
         }
     }
 }
